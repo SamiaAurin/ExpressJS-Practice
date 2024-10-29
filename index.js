5d3c5d1c360ca936588006b408830f81f615f057
Syeda Samia Sultana <107874016+Aurin-03@users.noreply.github.com>
2024-10-29 19:53:12 +0600

    Express JS Routing with GET method

--- index.js
+++ index.js
@@ -3,8 +3,21 @@ var express = require('express');
 app = express();
 
 app.get("/", function(req, res){
-    res.send("Hello Express JS");
+    res.send("Home Page");
 })
+
+app.get("/about", function(req, res){
+    res.send("About Page");
+})
+
+app.get("/contact", function(req, res){
+    res.send("Contact Page");
+})
+
+app.get("/terms", function(req, res){
+    res.send("Terms Page");
+})
+
 
 app.listen(8000, function() {
     console.log("Server Run Success");
