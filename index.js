99ccaa7073facf6193df0bc28a0869a7f58c08c4
Syeda Samia Sultana <107874016+Aurin-03@users.noreply.github.com>
2024-11-02 22:22:06 +0600

    JSON Response

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
 app = express();
+/*
 
 app.get("/", function(req, res){
     res.send("Home Page");
@@ -17,8 +18,27 @@ app.put("/contact", function(req, res){
 app.post("/terms", function(req, res){
     res.send("Terms Page");
 })
+*/
 
+
+app.get("/four", function(req, res){
+
+    let MyJSONArray = [
+        {
+            name: "Aurin",
+            city: "Dhaka",
+            occupation: "Engr."
+        },
+        {
+            name: "Samia",
+            city: "Dhaka",
+            occupation: "Chef"
+        },
+    ]
+
+    res.json(MyJSONArray);
+})    
 
 app.listen(8000, function() {
     console.log("Server Run Success");
-})
+}) 
